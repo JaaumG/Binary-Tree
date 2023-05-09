0d08139bd66105b472339f171057a6b192ca4123
JaaumG <jaaumgui@gmail.com>
2023-05-09 19:58:13 -0300

    Adicionado Struct de arvore

--- main.c
+++ main.c
@@ -2,7 +2,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+struct arv{
+	char info;
+	struct arv * esq;
+	struct arv * dir;
+};
+
+typedef struct arv * Arvore;
+
+Arvore iniciarArvore(){
+    return NULL;
+}
+
+Arvore criarNo(char info, Arvore esquerdo, Arvore direito){
+    Arvore arvore = (Arvore) malloc(sizeof(Arvore));
+	if(arvore!=NULL){
+        arvore->info = info;
+        arvore->esq = esquerdo;
+        arvore->dir = direito;
+        return arvore;
+	}
+	return false;
+}
+
+
+
+
 int main() {
-    printf("Hello, World!\n");
+	Arvore A = iniciarArvore();
+    Arvore D = criarNo('d', iniciarArvore(), iniciarArvore());
+    Arvore E = criarNo('e', iniciarArvore(), iniciarArvore());
+    Arvore F = criarNo('f', iniciarArvore(), iniciarArvore());
+    Arvore B = criarNo('b', iniciarArvore(), D);
+    Arvore C = criarNo('c', E, F);
+    A = criarNo('a', B, C);
+
+
+
     return 0;
 }
